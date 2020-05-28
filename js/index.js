0c4be147c24387c6fbfc48daee09a79fa801610f
it_dog <it_dog_zhang@163.com>
2020-05-28 21:12:50 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// const colors = ["red", "green", "blue", "yellow", "purple"];
+// function test() {
+//      colors.reduce(function (pre,cru,index,array) {
+//           console.log(pre,cru,index,array)
+//      })
+// }
+'use strict'
+// const obj = {
+//
+// }
+// Object.defineProperty(obj,"name",{
+//      value: 'test',
+//      writable: false
+//
+// })
+// function test(number) {
+//      obj["name"] = 'xx'
+//     console.log(obj)
+// }
+// jQuery.fn.extend({
+//     dogWang: function () {
+//          console.log("汪汪")
+//     }
+// });
+// jQuery.extend({
+//     dogWang: function () {
+//         console.log("汪汪")
+//     }
+// })
+// jQuery.dogWang = function () {
+//     console.log("wangwang")
+// }
+jQuery.prototype.dogWang = function (val) {
+    console.log(val)
+}
